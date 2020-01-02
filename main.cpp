00eec49ec49a09b4ea479d3b4ab271a9b80d69b9
kwh44 <maistruk44@gmail.com>
2020-01-02 17:47:46 +0200

    Add .img read

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <iterator>
+#include <vector>
+#include <string>
 
 int main(int argv, char *argc[]) {
+    
+    if (argv < 2) return -1;
+
+    std::ifstream input(std::string(argc[1]), std::ios::binary);
+
+    std::vector<char> bytes(
+         (std::istreambuf_iterator<char>(input)),
+         (std::istreambuf_iterator<char>()));
+    
+    input.close();
+    
     return 0;
 }
